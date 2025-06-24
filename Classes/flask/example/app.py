e8b86c615e172b976f2186fa59c3d79df9b214fd
Raphael Loeffler <raphael.loeffler2010@gmail.com>
2025-06-24 14:47:18 +0200

    structure for flask exercise

---
+++ Classes/flask/example/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/thank_you')
+def thank_you():
+    name: str = request.args.get('names')
+    password: str = request.args.get('password')
+    return render_template('thank_you.html', name=name, password=password)
+
+if __name__ == '__main__':
+    app.run(debug=True)
